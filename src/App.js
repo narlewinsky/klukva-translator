d41a514ba075c7fe4a0d3a933bb029775c59c4bc
Nar Lewinsky <33840321+narlewinsky@users.noreply.github.com>
2021-03-25 01:32:39 +0300

    Perenesön dizajn veb-versii

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import logo from './media/logo.svg';
+import Board from './Board';
 
 function App() {
     return (
         <div className="App">
-
+            <div className="Background" />
+            <img className="Logo" src={logo} />
+            <div className="Header">
+                <div className="Header-Content">
+                    <h1>PEREVOD NA RUSSKUJU LATINICU</h1>
+                    <p>Perevodi legko russkij tekst s kirillicy na sovremennyj projekt latinicy, osnovannyj na rezuljtatax raboty komissii po razrabotke projekta o latinizacii russkogo alfavita vo glave s professorom N. F. Jakovlevym.</p>
+                </div>
+            </div>
+            <Board />
         </div>
     );
 }
---
+++ src/Board.js
@@ -0,0 +1,57 @@
+import './Board.css';
+import React, { Component } from 'react';
+
+class Board extends Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = { content: "" }
+    }
+
+    handleChange(e) {
+        // const textarea = document.querySelector('.Board-Preview-TextArea');
+        // textarea.scrollTop = textarea.scrollHeight;
+        this.setState({
+            content: e.target.value
+        });
+    }
+
+    upload() {
+    }
+
+    download() {
+    }
+
+    copy() {
+    }
+
+    render() {
+        return (
+            <div className="Board">
+                <div className="Board-Panel">
+                    <div className="Board-Panel-Editor">
+                        <textarea className="Board-Editor-TextArea"
+                                  placeholder="Введите сюда текст на кириллице"
+                                  onChange={this.handleChange} />
+                    </div>
+                    <div className="Board-Panel-Separator" />
+                    <div className="Board-Panel-Toolbar">
+                    </div>
+                </div>
+                <div className="Board-Space" />
+                <div className="Board-Panel">
+                    <div className="Board-Panel-Editor">
+                        <textarea className="Board-Editor-TextArea"
+                                  placeholder="Введите сюда текст на кириллице"
+                                  onChange={this.handleChange} />
+                    </div>
+                    <div className="Board-Panel-Separator" />
+                    <div className="Board-Panel-Toolbar">
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Board;
